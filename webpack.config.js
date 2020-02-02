49f251509061143828c2635260deca610de36300
aslepenkov <alexmichel249@gmail.com>
2020-02-02 22:42:40 +0700

    inital

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const SRC_DIR = path.join(__dirname, './src');
+const DIST_DIR = path.join(__dirname, './dist');
+
+module.exports = {
+    entry: path.join(SRC_DIR, 'index.jsx'),
+    output: {
+        path: DIST_DIR,
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    resolve: {
+        modules: ['src', 'node_modules'],
+        extensions: ['*', '.js', '.jsx', '.json']
+    },
+    devServer: {
+        contentBase: DIST_DIR,
+        compress: false,
+        port: 9000,
+        historyApiFallback: true,
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({ template: 'src/index.html' })
+    ]
+};
